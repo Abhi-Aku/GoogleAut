327c9caae27bd14f7e636af6a7b9af8b447b54db
Abhi-Aku <abhi2003av@gmail.com>
2025-03-22 22:30:36 +0530

    Data
    UpDate

--- GoogleAuth/src/App.jsx
+++ GoogleAuth/src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About.Jsx';
+import Contect from './Components/Contect';
+
 
 const App = () => {
   return (
-<>
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-</> 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        <Route path="/contact" element={<Contect />} />
+        <Route path="/about" element={<About />} />
+        
+        </Route>
+        
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-)}
-
-export default App
+export default App;
